f1e84246164660e57b74958d0c1b6cb3e595c778
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-03-30 14:58:18 +0600

    Have added files for the project

---
+++ src/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Caching.Demo.Models;
+
+namespace AspNetCore.Caching.Demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return new WeatherForecastRandomCollection(1, 5);
+        }
+    }
+}
---
+++ src/Models/WeatherForecastRandomCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AspNetCore.Caching.Demo.Models
+{
+    public class WeatherForecastRandomCollection : IReadOnlyCollection<WeatherForecast>
+    {
+        private static readonly string[] _summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IReadOnlyCollection<WeatherForecast> _collection;
+
+        public WeatherForecastRandomCollection(int @from, int to)
+        {
+            var random = new Random();
+
+            _collection = Enumerable.Range(@from, to).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = random.Next(-20, 55),
+                Summary = _summaries[random.Next(_summaries.Length)]
+            }).ToArray();
+        }
+
+        public IEnumerator<WeatherForecast> GetEnumerator() => _collection.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count => _collection.Count;
+    }
+}
